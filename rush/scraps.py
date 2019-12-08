dcf221246ac38fd7cdb4ce39001a36ad1ebe80c2
rednafi <redowan.nafi@gmail.com>
2019-12-09 00:05:32 +0600

    Click experimentation

--- rush/scraps.py
+++ rush/scraps.py
@@ -1,19 +1,43 @@
-a = {
-    "task_1": ['echo "task1 is running"', "ls"],
-    "task_2": ['echo "task2 is running"'],
-    "task_3": ["task1", "task2"],
-    "task_4": ["task3"],
-}
+import click
+
+# + Executing random/entrypoints:
+
+import sys
+
+import colorama
+import click
+
+# Don't strip colors.
+colorama.init(strip=False)
 
 
-def find(dictionary):
-    for k, v in dictionary.items():
-        if isinstance(v, list):
-            for i in v:
-                if isinstance(i, str) and i == k:
-                    dictionary[k] = v
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.option(
+    "--read-stderr", is_flag=True, type=click.BOOL, default=True, help="Read stderr."
+)
+@click.option(
+    "--no-color", is_flag=True, type=click.BOOL, default=False, help="Read stderr."
+)
+@click.option(
+    "--color", nargs=1, type=click.STRING, default="green", help="Color to use."
+)
+@click.option("--char", nargs=1, type=click.STRING, default="--", help="Prefix char.")
+def entrypoint1(*, char, read_stderr, no_color, color):
+    """Indents and echoes string with a specific pipe."""
 
-                yield dictionary
+    pipe = sys.stdin if not read_stderr else sys.stderr
 
+    if no_color:
+        color = "white"
 
-print(list(find(a)))
+    for line in pipe:
+        if line:
+            title = str(click.style(line, fg=color))
+            print(f" {char} ", end="")
+            print(title.rstrip())
+        else:
+            print(f" {char} ", end="")
+
+import subprocess
+m = entrypoint1()
+subprocess.run(m)
