0d083b8fa2c219876a616dfea04a8e69e6bcdef6
rednafi <rednafi@pop-os.localdomain>
2019-12-15 19:47:55 +0600

    Term beautify

--- rush/script.py
+++ rush/script.py
@@ -2,50 +2,74 @@ import yaml
 from collections import OrderedDict
 import os
 import subprocess
+import sys
+import colorama
+import click
 
 from typing import Dict, List
 from rush.utils import strip_spaces, split_lines, echo_underlines, run_task
 
+# Don't strip colors.
+colorama.init(strip=False)
+
 
 def _read_yml():
-    if os.path.exists("rushfile.yml"):
+    try:
         with open("./rushfile.yml") as file:
             yml_content = yaml.load(file, Loader=yaml.FullLoader)
         return yml_content
 
-    else:
-        raise FileNotFoundError("rushfile.yml file not found")
+    except FileNotFoundError:
+        sys.exit("rushfile.yml file not found")
 
 
 def _clean_tasks(yml_content):
     cleaned_tasks = OrderedDict()
+    try:
+        for task_name, task_chunk in yml_content.items():
+            task_chunk = strip_spaces(task_chunk)
+            task_chunk = split_lines(task_chunk)
+            cleaned_tasks[task_name] = task_chunk
 
-    for task_name, task_chunk in yml_content.items():
-        task_chunk = strip_spaces(task_chunk)
-        task_chunk = split_lines(task_chunk)
-        cleaned_tasks[task_name] = task_chunk
-
-    return cleaned_tasks
+        return cleaned_tasks
+    except AttributeError:
+        sys.exit("Rushfile is empty.")
 
 
-def _run_task_chunk(cleaned_tasks, *task_names):
-    if task_names:
-        cleaned_tasks = {k: cleaned_tasks[k] for k in task_names}
+def _filter_tasks(cleaned_tasks, *filter_names):
+    if filter_names:
+        try:
+            filtered_tasks = {k: cleaned_tasks[k] for k in filter_names}
+            return filtered_tasks
+        except KeyError:
+            sys.exit("Task does not exist.")
+    else:
+        return cleaned_tasks
 
+
+def _term_beautify(task_name):
+    task_name = f"{task_name}:"
+    task_name = str(click.style(task_name, fg="yellow"))
+    separator_len = len(task_name) + 3
+    separator = str(click.style("=", fg="green")) * separator_len
+    
+    print("")
+    print(task_name)
+    print(separator)
+
+
+def _run_task_chunk(cleaned_tasks):
     for task_name, task_chunk in cleaned_tasks.items():
-        print_task_name = f"Executing {task_name}"
-        print("" * len(print_task_name))
-        print(print_task_name)
-        print("=" * len(print_task_name))
-
+        _term_beautify(task_name)
         for task in task_chunk:
             run_task(task)
 
 
-def run_all_tasks(*task_names):
+def run_all_tasks(*filter_names):
     yml_content = _read_yml()
     cleaned_tasks = _clean_tasks(yml_content)
-    _run_task_chunk(cleaned_tasks, *task_names)
+    filtered_tasks = _filter_tasks(cleaned_tasks, *filter_names)
+    _run_task_chunk(filtered_tasks)
 
 
-run_all_tasks('task_1')
+run_all_tasks("task_1", "task_3")
