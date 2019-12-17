2fbbbe62f78c9e7c3c80c431321e3476de1589b8
rednafi <redowan.nafi@gmail.com>
2019-12-17 18:09:11 +0600

    Fixed options

--- rush/cli.py
+++ rush/cli.py
@@ -13,10 +13,20 @@ colorama.init(strip=False)
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option(
-    "--print-command", default=True, help="Option to print/supress the commands."
+    "--print_command", default=True, help="Option to print/supress the commands."
 )
-@click.option("--capture-error", default=True, help="Option to capture/ignore error")
+@click.option("--capture_error", default=True, help="Option to capture/ignore error")
 @click.argument("filter_names", required=False, nargs=-1)
 def entrypoint(*, filter_names, print_command, capture_error):
-    run_all_tasks(*filter_names, is_print_cmd=True, capture_error=False)
 
+    if print_command == "y":
+        is_print_cmd = True
+    elif print_command == "n":
+        is_print_cmd = False
+
+    if capture_error == "y":
+        capture_error = True
+    elif capture_error == "n":
+        capture_error = False
+
+    run_all_tasks(*filter_names, is_print_cmd=is_print_cmd, capture_error=capture_error)
