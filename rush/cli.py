a4f041af7728081c32b5136bbbe10a363d1a007e
rednafi <redowan.nafi@gmail.com>
2019-12-18 13:35:09 +0600

    Better boolean options

--- rush/cli.py
+++ rush/cli.py
@@ -13,20 +13,26 @@ colorama.init(strip=False)
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option(
-    "--print_command", default=True, help="Option to print/supress the commands."
+    "--print-cmd/--not-print-cmd",
+    default=True,
+    help="Option to print/supress the commands.",
 )
-@click.option("--capture_error", default=True, help="Option to capture/ignore error")
+@click.option(
+    "--capture-err/--not-capture-err",
+    default=True,
+    help="Option to capture/ignore error",
+)
 @click.argument("filter_names", required=False, nargs=-1)
-def entrypoint(*, filter_names, print_command, capture_error):
+def entrypoint(*, filter_names, print_cmd, capture_err):
 
-    if print_command == "y":
-        is_print_cmd = True
-    elif print_command == "n":
-        is_print_cmd = False
+    # if print_command == "y":
+    #     is_print_cmd = True
+    # elif print_command == "n":
+    #     is_print_cmd = False
 
-    if capture_error == "y":
-        capture_error = True
-    elif capture_error == "n":
-        capture_error = False
+    # if capture_error == "y":
+    #     capture_error = True
+    # elif capture_error == "n":
+    #     capture_error = False
 
-    run_all_tasks(*filter_names, is_print_cmd=is_print_cmd, capture_error=capture_error)
+    run_all_tasks(*filter_names, print_cmd=print_cmd, capture_err=capture_err)
--- rush/script.py
+++ rush/script.py
@@ -70,24 +70,21 @@ def _term_beautify(task_name, is_color=True, is_task_name=True):
         click.echo(f"{separator} {task_name}")
 
 
-def _run_task_chunk(cleaned_tasks, is_print_cmd=True, capture_error=True):
+def _run_task_chunk(cleaned_tasks, print_cmd=True, capture_err=True):
     for task_name, task_chunk in cleaned_tasks.items():
         _term_beautify(task_name)
         for cmd in task_chunk:
-            if is_print_cmd and cmd.startswith('#') is False:
+            if print_cmd and cmd.startswith('#') is False:
                 _term_beautify(cmd, is_task_name=False)
             try:
-                run_task(cmd, capture_error)
+                run_task(cmd, capture_err)
             except subprocess.CalledProcessError as e:
                 click.echo(e)
                 sys.exit(1)
 
 
-def run_all_tasks(*filter_names, is_print_cmd, capture_error):
+def run_all_tasks(*filter_names, print_cmd, capture_err):
     yml_content = _read_yml()
     cleaned_tasks = _clean_tasks(yml_content)
     filtered_tasks = _filter_tasks(cleaned_tasks, *filter_names)
-    _run_task_chunk(filtered_tasks, is_print_cmd, capture_error)
-
-
-
+    _run_task_chunk(filtered_tasks, print_cmd, capture_err)
