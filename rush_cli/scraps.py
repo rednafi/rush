c9fa22f5438c0f7288ce1c6ac77b51e065a7537d
rednafi <redowan.nafi@gmail.com>
2019-12-24 14:22:35 +0600

    Fixed ignore error bug

--- rush_cli/scraps.py
+++ rush_cli/scraps.py
@@ -1,58 +1,7 @@
-import os
-import click
-import sys
 import yaml
 
-
-def _check_rushfiles():
-    rushfiles = []
-    for file in os.listdir("./"):
-        if file.startswith("rushfile") and (
-            file.endswith(".yml") or file.endswith(".yaml")
-        ):
-            rushfiles.append(file)
-    return rushfiles
-
-
-def _read_yml():
-    rushfiles = _check_rushfiles()
-
-    if len(rushfiles) < 1:
-        sys.exit(
-            click.style(
-                "Error: Rushfile [rushfile.yml/rushfile.yaml] not found.", fg="magenta"
-            )
-        )
-    elif len(rushfiles) > 1:
-        sys.exit(
-            click.style(
-                "Error: Multiple rushfiles [rushfile.yml/rushfile.yaml]"
-                " in the same directory.",
-                fg="magenta",
-            )
-        )
-
-    try:
-        rushfile = rushfiles[0]
-
-        if rushfile.endswith(".yml"):
-            with open("./rushfile.yml") as file:
-                yml_content = yaml.load(file, Loader=yaml.FullLoader)
-            return yml_content
-
-        elif rushfile.endswith(".yaml"):
-            with open("./rushfile.yaml") as file:
-                yml_content = yaml.load(file, Loader=yaml.FullLoader)
-            return yml_content
-
-    except yaml.scanner.ScannerError:
-        sys.exit(
-            click.style("Error: rushfile.yml is not properly formatted", fg="magenta")
-        )
-
-
-rushfile = _check_rushfiles()[0]
-if rushfile.endswith(".yaml"):
-    with open("./rushfile.yaml") as file:
-        yml_content = yaml.load(file, Loader=yaml.FullLoader)
-print(yml_content)
+with open("./Rushfile") as file:
+    yml_content = yaml.load(file, Loader=yaml.FullLoader)
+    # make sure the task names are strings
+    yml_content = {str(k): v for k, v in yml_content.items()}
+    print(yml_content)
