71b4d9c2cf4753c71e9177e5db4b897ad6029ada
rednafi <redowan.nafi@gmail.com>
2019-12-31 00:05:06 +0600

    Full bash

--- rush_cli/utils.py
+++ rush_cli/utils.py
@@ -73,10 +73,11 @@ def find_shell_path(shell_name="bash"):
 def run_task(cmd, cmd_name, interactive=True, catch_error=True):
     use_shell = find_shell_path()
 
+    click.echo("")
     click.secho(f" {cmd_name}:", fg='yellow')
     click.secho(f" {'='*len(cmd_name)}", fg='green')
 
-    result = run(cmd, shell=use_shell, hide=True, warn=True)
+    result = run(cmd, shell=use_shell, hide=True, warn=True,)
 
     if interactive:
         for line in result.stdout.splitlines():
@@ -85,17 +86,25 @@ def run_task(cmd, cmd_name, interactive=True, catch_error=True):
         if not catch_error:
             for line in result.stderr.splitlines():
                 click.secho("  | " + line, fg='magenta')
+
         elif catch_error:
             for line in result.stderr.splitlines():
                 click.secho("  | " + line, fg='magenta')
-                click.echo('err')
                 sys.exit(1)
+    else:
+        if catch_error:
+            sys.exit(1)
+
+
 
 cmd = """
-git status
-git add .
-git commit -m "d"
+ec ls
 """
-print(run_task(cmd, "task_1", interactive=True, catch_error=False))
+m = ((cmd, "task_1"), (cmd, "task_2"))
+
+for t in m:
+    cmd, name = t
+    run_task(cmd, name, interactive=True, catch_error=False)
+
 # result = run(cmd, shell=find_shell_path(), hide=True, warn=True)
 # print(result.exited)
